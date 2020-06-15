c379a7cd40d5adef775d9a0bee6d968bf8ab138e
flaviammartini <flavinhamachadomartini@gmail.com>
2020-06-15 02:00:40 -0300

    create globalStyle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from "react";
+import Home from "../src/components/pages/Home";
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import theme from '../src/components/themes/default';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    background-color: #EDEDED !important;
+  }
+`
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+  <Fragment>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+    <Home />
+    </ThemeProvider>
+    </Fragment>
+  )
 }
 
 export default App;
