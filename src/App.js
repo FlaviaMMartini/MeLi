def68c66443e855f0b70f998af85f9c6fbe7a26e
flaviammartini <flavinhamachadomartini@gmail.com>
2020-06-16 00:30:46 -0300

    create history

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, {Fragment} from "react";
-import Home from "../src/components/pages/Home";
+import Details from "../src/components/Details";
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import theme from '../src/components/themes/default';
+import history from '../src/components/history';
+import { Router, Switch, Route } from 'react-router-dom';
+import SearchProducts from "./components/SearchProducts";
+import ListResults from '../src/components/ListResults';
+import Home from '../src/components/pages/Home';
+
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -14,7 +20,14 @@ function App() {
   <Fragment>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
-    <Home />
+    <Router history={history}>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/search" component={SearchProducts}  />
+            <Route path="/details/:id" component={Details} />
+            <Route path="/list" component={ListResults} />
+          </Switch>
+        </Router>
     </ThemeProvider>
     </Fragment>
   )
