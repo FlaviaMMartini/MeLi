9bd42ee19c3acfb00496b2adcb6366d63a088c5c
FlaviaMMartini <flavinhamachadomartini@gmail.com>
2020-06-17 12:07:24 -0300

    styles in Details

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -2,8 +2,15 @@ import React, { Fragment, useEffect, useState, useCallback } from "react";
 import { Tooltip, Card } from "antd";
 import { withRouter, useParams } from "react-router-dom";
 import useDataApi from "use-data-api";
-import { List, Row, Col, Button,Spin } from "antd";
+import { List, Row, Col, Button, Spin } from "antd";
 import SearchProducts from "../SearchProducts";
+import {
+  Wrapper,
+  RowCustom,
+  TitleDescription,
+  Title, TitlePrice,
+  ButtonCustom
+} from "./styles";
 
 
 const Details = () => {
@@ -19,36 +26,52 @@ const Details = () => {
 
   return (
     <>
-     <SearchProducts/>
-    {isError && (
-      <Col
-        span={24}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginTop: "3rem",
-        }}
-      >
-        <h2>Ops! algo deu errado, verifique sua conexão...</h2>
-      </Col>
-    )}
-    {isLoading ? (
-      <Col
-        span={24}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          marginTop: "3rem",
-        }}
-      >
-        <Spin tip="Buscando..." />
-      </Col>
-    ) : (
-      <div> {data.title}</div> 
-    )}
-</>
+      <SearchProducts />
+      {isError && (
+        <Col
+          span={24}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "3rem",
+          }}
+        >
+          <h2>Ops! algo deu errado, verifique sua conexão...</h2>
+        </Col>
+      )}
+      {isLoading ? (
+        <Col
+          span={24}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "3rem",
+          }}
+        >
+          <Spin tip="Buscando..." />
+        </Col>
+      ) : (
+          <Wrapper>
+            <RowCustom>
+              <Col style={{textAlign: 'center'}} xs={24} sm={24} md={15} lg={15} xl={15} >
+                <img width={400} alt="logo" src={data.thumbnail} />
+                <TitleDescription>Descrição do produto</TitleDescription>
+                <h3>{data.description}</h3>
+              </Col>
+              <Col xs={24} sm={24} md={9} lg={9} xl={9} >
+                <span> {data.condition}&nbsp;-&nbsp;{data.sold_quantity}&nbsp;vendidos&nbsp;</span>
+                <Title>{data.title}</Title>
+                <TitlePrice>$ {data.price}</TitlePrice>
+                <ButtonCustom type="primary" >
+                  Comprar
+                </ButtonCustom>
+              </Col>
+            </RowCustom>
+          </Wrapper>
+        )}
+    </>
   );
 };
 
---
+++ src/components/Details/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import {Row, Button} from "antd";
+
+export const Wrapper = styled.div`
+padding-right: 20%;
+padding-left: 20%;
+padding-top:3%;
+margin-bottom:1%;
+}
+`;
+export const RowCustom = styled(Row)`
+width:100%;
+height: 50rem;
+background-color: #ffffff;
+padding: 2%;
+}
+`;
+export const Title = styled.p`
+margin-top:1%;
+font-weight:bold;
+fontd-size:2rem;
+}
+`;
+export const TitlePrice = styled.p`
+margin-top:5%;
+font-size:3rem;
+}
+`;
+
+export const TitleDescription = styled.p`
+padding-top: 6%;
+font-size:2rem;
+float: left;
+padding-left:1%
+}
+`;
+
+export const ButtonCustom = styled(Button)`
+width:100%;
+}
+`;
