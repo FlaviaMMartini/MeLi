4678efe2e8c03c53092def7205da334f2306b043
FlaviaMMartini <flavinhamachadomartini@gmail.com>
2020-06-17 12:08:01 -0300

    modify styles in ListResults

--- src/components/ListResults/index.js
+++ src/components/ListResults/index.js
@@ -1,56 +1,58 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { List, Row, Col, Button } from "antd";
 import { withRouter } from "react-router-dom";
-import {Wrapper, RowCustom} from './styles';
+import { Wrapper, RowCustom, Title, TitlePrice, TextCurrency } from './styles';
+import Shipping from '../../assets/ic_shipping.png';
 
 const ListResults = ({ data, history }) => {
-  
+  const[category,setCategory]=useState('');
+
   const next = (item) => {
     console.log('esse e o history', history);
     console.log(item);
     history.push({ pathname: `/details/${item}` });
     history.go({ pathname: `/details/${item}` });
   };
+
   return (
     <Wrapper>
-          <List
-          pagination={{
-            onChange: (page) => {
-              console.log(page);
-            },
-            pageSize: 4,
-          }}
-          itemLayout="horizontal"
-          dataSource={data.results}
-          renderItem={(item) => (
-            <List.Item  style={{backgroundColor:"#ffffff", padding:'2%'}} onClick={() => next(item.id)}>
-              {/* <List.Item.Meta /> */}
-              <RowCustom>
-                <Col xs={24} sm={24} md={6} lg={6} xl={6} >
-              <img width={200} alt="logo" src={item.thumbnail} />
+    {data.results.title}
+      <List
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 4,
+        }}
+        itemLayout="horizontal"
+        dataSource={data.results}
+        renderItem={(item) => (
+          <List.Item style={{ backgroundColor: "#ffffff", padding: '2%' }} onClick={() => next(item.id)}>
+            <RowCustom>
+              <Col xs={24} sm={24} md={6} lg={6} xl={6} >
+                <img width={200} alt="logo" src={item.thumbnail} />
               </Col>
-              <Col xs={24} sm={24} md={13} lg={13} xl={13}>
-                  <Row>
-                  <Col span={8}>
-                  {item.price}
+              <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+                <Row>
+                  <Col span={2}>
+                    <TextCurrency>$</TextCurrency>
                   </Col>
-                  <Col span={8}>
-                  {item.price}
-                  </Col>
-                  <Col span={8}>
-                  {item.price}
-                  </Col>
-                  </Row>
-              {item.title}
+                  <Col span={4}>
+                    <TitlePrice>{item.price}</TitlePrice>
+                  </Col >{item.shipping.free_shipping && (<Col style={{ marginTop: '3%' }} span={4}>
+                    <img src={Shipping} />
+                  </Col>)}
+                </Row>
+                <Title>{item.title}</Title>
               </Col>
-              <Col xs={24} sm={24} md={3} lg={3} xl={3}>
-              {item.address.state_name}
+              <Col xs={24} sm={24} md={3} lg={3} xl={3} style={{ marginTop: '7%' }}>
+                {item.address.state_name}
               </Col>
-              </RowCustom>
-            </List.Item>
-          )}
-        />
-        </Wrapper>
+            </RowCustom>
+          </List.Item>
+        )}
+      />
+    </Wrapper>
   );
 };
 
--- src/components/ListResults/styles.js
+++ src/components/ListResults/styles.js
@@ -5,9 +5,23 @@ export const Wrapper = styled.div`
 padding-right: 20%;
 padding-left: 20%;
 padding-top:3%;
+margin-bottom:1%;
 }
 `;
 export const RowCustom = styled(Row)`
 width:100%;
+}
+`;
+export const Title = styled.h2`
+margin-top:1%;
+}
+`;
+export const TitlePrice = styled.h2`
+margin-top:12%;
+}
+`;
+
+export const TextCurrency = styled.h1`
+padding-top: 6%;
 }
 `;
