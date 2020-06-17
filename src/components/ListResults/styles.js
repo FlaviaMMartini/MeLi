e1b5165350fd4a725ed95c95d6e14fd1effd3f58
FlaviaMMartini <flavinhamachadomartini@gmail.com>
2020-06-16 21:40:53 -0300

    modify style ListResults

---
+++ src/components/ListResults/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import {Row} from "antd";
+
+export const Wrapper = styled.div`
+padding-right: 20%;
+padding-left: 20%;
+padding-top:3%;
+}
+`;
+export const RowCustom = styled(Row)`
+width:100%;
+}
+`;
